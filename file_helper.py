b92b92a6aa7c17caadf38a7a4c4308336a5affe0
Vitali Hanich <vitali.hanich@gmail.com>
2019-12-10 02:41:20 -0500

    Updated datetime format and empty file size

--- file_helper.py
+++ file_helper.py
@@ -5,7 +5,7 @@ from log_helper import init_logger
 logger = init_logger()
 
 
-def check_file_is_empty(file, size_limit=6000):
+def check_file_is_empty(file, size_limit=7000):
     if os.path.getsize(file) < size_limit:
         os.remove(file)
         logger.warning('Report file \'{}\' is empty, email was not sent'.format(file))
