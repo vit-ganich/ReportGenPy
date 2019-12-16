235d4936676f5ac0321234ae34bf90d5ff5bdd72
Vitali Hanich <vitali.hanich@gmail.com>
2019-12-16 07:25:05 -0500

    Added dictionary creation

--- OOP/version.py
+++ OOP/version.py
@@ -1,5 +1,5 @@
 import glob
-import traceback
+import json
 import xml.etree.ElementTree as elTree
 
 path = '\\\\testresultrepo.csiqa.local\\TestResults\\CI\\V8'
@@ -9,42 +9,27 @@ class Version:
 
     def __init__(self, path_to_ver):
         self.version = path_to_ver
-        #self.projects_list = self.get_projects()
+        # self.projects_list = self.get_projects()
 
     def get_projects(self):
-        ret = {}
-        # for project_path in glob.glob(self.path_to_ver + '\\*', recursive=True):
-        #     proj_name = project_path.split('\\')[-1]
-        #     ret.update({proj_name: Project(project_path)})
-        # return ret
         return [Project(project) for project in glob.glob(self.version + '\\*', recursive=True)]
 
 
 class Project:
     def __init__(self, path_to_proj):
         self.project = path_to_proj
-        #self.themes_list = self.get_themes()
+        # self.themes_list = self.get_themes()
 
     def get_themes(self):
-        # ret = {}
-        # for theme_path in glob.glob(self.path_to_proj + '\\*', recursive=True):
-        #     theme_name = theme_path.split('\\')[-1]
-        #     ret.update({theme_name: Theme(theme_path)})
-        # return ret
         return [Theme(theme) for theme in glob.glob(self.project + '\\*', recursive=True)]
 
 
 class Theme:
     def __init__(self, path_to_day):
         self.theme = path_to_day
-        #self.days_list = self.get_days()
+        # self.days_list = self.get_days()
 
     def get_days(self):
-        # ret = {}
-        # for day_path in glob.glob(self.path_to_day + '\\*', recursive=True):
-        #     day_name = day_path.split('\\')[-1]
-        #     ret.update({day_name: Day(day_path)})
-        # return ret
         return [Day(day) for day in glob.glob(self.theme + '\\*', recursive=True)]
 
 
@@ -59,19 +44,8 @@ class Day:
     def __init__(self, path_to_features):
         self.daily_folder = path_to_features
         self.stats_for_day = self.get_daily_stats()
-        #self.features_list = self.get_features()
-
 
     def get_features(self):
-        # ret = {}
-        # for feature_path in glob.glob(self.path_to_features + '\\*\\*.trx', recursive=True):
-        #     feature_name = feature_path.split('\\')[-1]
-        #
-        #     feature = Feature(feature_path)
-        #     if feature.passed:
-        #         self.passed += 1
-        #     ret.update({feature_name: Feature(feature_path)})
-        # return ret
         self.features_list = [Feature(feature) for feature in glob.glob(self.daily_folder + '\\*\\*.trx',
                                                                         recursive=True)]
         return self.features_list
@@ -82,7 +56,6 @@ class Day:
             split_path_to_ff = ff.split('\\')
             trx_name = split_path_to_ff[-1]
             ff_info = [item.replace('.trx', '') for item in trx_name.split('_')]
-
             # Parts of feature info
             tail = ff_info[-1].split('-')
             results = tail[1:]
@@ -90,7 +63,6 @@ class Day:
             if not failed:
                 passed_scen += 1
         return passed_scen
-
 
     def get_daily_stats(self):
         self.total_scenarios = len(glob.glob(self.daily_folder + '\\*\\*.trx'))
@@ -101,6 +73,7 @@ class Day:
         else:
             self.percentage = 0
         return [self.total_scenarios, self.passed, self.failed, self.percentage]
+
 
 class Feature:
     feature = ''
@@ -160,9 +133,6 @@ class Feature:
                     error_info = children[1]
                     message = list(error_info)[0]
                     stack_trace = list(error_info)[1]
-                    #for errorInfo in output:
-                    #for errorInfo in std_out:
-                        #error_list = [item for item in errorInfo.text.split('\n')]
                     error_list = [line for line in std_out.text.split('\n')]
                     if len(error_list) == 1:
                         # error_message = errorInfo.text
@@ -190,26 +160,32 @@ class Scenario:
     def __init__(self):
         pass
 
-import json
+
+final_dict = dict()
 v8 = Version(path)
 for project in v8.get_projects():
-
-    json_data = json.dumps(project.__dict__)
-    print(json_data)
-    for theme in project.get_themes():
-        print(json.dumps(theme.__dict__))
-        for day in theme.get_days():
-            print(json.dumps(day.__dict__))
+    key = project.project.split('\\')[-1]
+    final_dict.update({key: {}})
+    for theme_inst in project.get_themes():
+        theme_name = theme_inst.theme.split('\\')[-1]
+        theme_dict = {theme_name: {}}
+        final_dict[key].update(theme_dict)
+        for day in theme_inst.get_days():
+            day_str = day.daily_folder.split('\\')[-1]
+            final_dict[key][theme_name].update({day_str: []})
             for feature in day.get_features():
-                print(json.dumps(feature.__dict__))
+                feature_str = feature.info
+                final_dict[key][theme_name][day_str].append(feature_str)
 
 
-#projects = v8.projects_list
+#print(final_dict)
+# with open('result.json', 'w') as file:
+#     json.dump(final_dict, file)
+
+# with open('result.json', 'r') as file:
+#     data = json.load(file)
 
 
-# debug = r'\\testresultrepo.csiqa.local\TestResults\CI\V8\FOUNDATION\Horizon\11_30_2019\CEP CM_2_CLTQACLIENT492\90.14-Package Approvals-PLM_Desktop_Horizon_Oracle_Chrome_B8.1.19334.1-91-57-56-0-1.trx'
-# feature = Feature(debug)
-# json_data = json.dumps(feature.__dict__)
-# print(json_data)
+import pandas
 
-
+pandas.read_json('result.json').to_excel('output.xlsx')
